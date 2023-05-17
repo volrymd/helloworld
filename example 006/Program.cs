f8f8612e66103000994c8b6dc9507d3aacc0f1fb
JaneZan <volrymd@gmail.com>
2023-05-17 21:52:17 +0300

    create example 006

---
+++ example 006/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите Ваше Имя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "маша") 
+{
+    Console.WriteLine ("Ура, это же Маша!");
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine (username);
+}
